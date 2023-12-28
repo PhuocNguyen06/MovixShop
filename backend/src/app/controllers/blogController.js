1d6e83ef8d693541944438e8db5b2e313ceb533f
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2023-12-29 00:46:18 +0700

    featured dislike the blog

--- backend/src/app/controllers/blogController.js
+++ backend/src/app/controllers/blogController.js
@@ -116,11 +116,60 @@ const liketheBlog = asyncHandler(async (req, res) => {
   }
 });
 
+//dislike the blog
+const disliketheBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.body;
+  validateMongoDbId(blogId);
+  // Find the blog which you want to be liked
+  const blog = await Blog.findById(blogId);
+  // find the login user
+  const loginUserId = req?.user?._id;
+  // find if the user has liked the blog
+  const isDisliked = blog?.isDisliked;
+  // find if the user has disliked the blog
+  const alreadyLiked = blog?.likes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString()
+  );
+  if (alreadyLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    );0
+    res.json(blog);
+  }
+  if (isDisliked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: loginUserId },
+        isDisliked: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  } else {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { dislikes: loginUserId },
+        isDisliked: true,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+});
+
 module.exports = {
   createBlog,
   getaBlog,
   getAllBlog,
   updatedBlog,
   deletedBlog,
-  liketheBlog
+  liketheBlog,
+  disliketheBlog
 };
--- backend/src/app/routes/blogRoute.js
+++ backend/src/app/routes/blogRoute.js
@@ -6,7 +6,8 @@ const {
     getAllBlog,
     updatedBlog,
     deletedBlog,
-    liketheBlog
+    liketheBlog,
+    disliketheBlog
 } = require("../controllers/blogController")
 const router = express.Router();
 
@@ -14,7 +15,8 @@ const router = express.Router();
 router.post("/", authMiddleware,isAdmin, createBlog);
 router.get("/:id", getaBlog);
 router.get("/", getAllBlog);
-router.put("/likes", authMiddleware, liketheBlog)
+router.put("/likes", authMiddleware, liketheBlog);
+router.put("/dislikes", authMiddleware, disliketheBlog)
 router.put("/:id", authMiddleware, isAdmin, updatedBlog);
 router.delete("/:id", authMiddleware, isAdmin, deletedBlog );
 
