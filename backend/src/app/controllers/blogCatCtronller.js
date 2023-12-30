d657f4389b54790ac8cfe80a885129a48af98e7e
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2023-12-30 17:04:28 +0700

    update category

--- backend/src/app/controllers/blogCatCtronller.js
+++ backend/src/app/controllers/blogCatCtronller.js
@@ -3,67 +3,66 @@ const asyncHandler = require("express-async-handler");
 
 //create a new category
 const createCategory = asyncHandler(async (req, res) => {
-    try {
-      const newCategory = await Category.create(req.body);
-      res.json(newCategory);
-    } catch (error) {
-      throw new Error(error);
-    }
-  });
+  try {
+    const newCategory = await Category.create(req.body);
+    res.json(newCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 //get a category
 const getaCategory = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    validateMongoDbId(id);
-    try {
-      const getaCategory = await Category.findById(id);
-      res.json(getaCategory);
-    } catch (error) {
-      throw new Error(error);
-    }
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaCategory = await Category.findById(id);
+    res.json(getaCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 // get all category
 const getAllCategory = asyncHandler(async (req, res) => {
-    try {
-      const getallCategory = await Category.find();
-      res.json(getallCategory);
-    } catch (error) {
-      throw new Error(error);
-    }
+  try {
+    const getallCategory = await Category.find();
+    res.json(getallCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 //update a category
 const updatedCategory = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    validateMongoDbId(id);
-    try {
-      const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
-      res.json(updatedCategory);
-    } catch (error) {
-      throw new Error(error);
-    }
-  });
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 // delete a category
 const deletedCategory = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    validateMongoDbId(id);
-    try {
-      const deletedCategory = await Category.findByIdAndDelete(id);
-      res.json(deletedCategory);
-    } catch (error) {
-      throw new Error(error);
-    }
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    res.json(deletedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
-
 module.exports = {
-    createCategory,
-    getaCategory,
-    getAllCategory,
-    updatedCategory,
-    deletedCategory
-}
+  createCategory,
+  getaCategory,
+  getAllCategory,
+  updatedCategory,
+  deletedCategory,
+};
