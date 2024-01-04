e146269fc8346a730f9d627c41465b40f43e8464
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-04 23:06:14 +0700

    featured delete enquiry

--- backend/src/app/controllers/enqController.js
+++ backend/src/app/controllers/enqController.js
@@ -26,7 +26,22 @@ const updateEnqiry = asynHandler(async (req, res) => {
       throw new Error(error);
     }
 });
+
+//delete a Enqiry
+
+const deleteEnquiry = asynHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+      const deletedEnquiry = await Coupon.findByIdAndDelete(id);
+      res.json(deletedEnquiry);
+    } catch (error) {
+      throw new Error(error);
+    }
+});
+
 module.exports = {
   createEnqiry,
-  updateEnqiry
+  updateEnqiry,
+  deleteEnquiry
 };
--- backend/src/app/routes/enqRoute.js
+++ backend/src/app/routes/enqRoute.js
@@ -3,11 +3,13 @@ const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
 const {
     createEnqiry,
     updateEnqiry,
-    
+    deleteEnquiry,
+
 } = require("../controllers/enqController");
 const router = express.Router();
 router.post("/", authMiddleware, isAdmin, createEnqiry);
 router.put("/:id", authMiddleware, isAdmin, updateEnqiry);
+router.delete("/:id", authMiddleware, isAdmin, deleteEnquiry);
 
 
 module.exports = router;
