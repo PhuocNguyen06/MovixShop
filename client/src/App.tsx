579f4a42f0b94f1856c5798e597046be7958bfd2
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-08 10:47:48 +0700

    base frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
