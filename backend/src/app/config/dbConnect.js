f2d3de8218400602c00e1935c202fa06d96f655b
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-24 12:00:10 +0700

    update connect db

--- backend/src/app/config/dbConnect.js
+++ backend/src/app/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("connect successfully");
   } catch (error) {
     console.log("Failed", error);
