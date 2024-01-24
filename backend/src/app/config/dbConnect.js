4ff5ee1f1b4885db698ed07bfedec6455b930f2d
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-24 12:11:53 +0700

    update func connect db

--- backend/src/app/config/dbConnect.js
+++ backend/src/app/config/dbConnect.js
@@ -2,11 +2,34 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log("connect successfully");
+    const connection = await mongoose.connect(process.env.MONGODB_URL);
+    if (connection) console.log("connect successfully");
   } catch (error) {
-    console.log("Failed", error);
+    if (error instanceof Error) {
+      console.log(`Error: ${error.message}`);
+    }
+    process.exit(1);
   }
 };
 
 module.exports = dbConnect;
+
+// const mongoose = require("mongoose");
+
+// const dbConnect = async () => {
+//   try {
+//     const connection =
+//       process.env.MONGODB_URL &&
+//       (await mongoose.connect(process.env.MONGODB_URL));
+//     if (connection) {
+//       console.log("connect successfully");
+//     }
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       console.log(`Error: ${error.message}`);
+//     }
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = dbConnect;
