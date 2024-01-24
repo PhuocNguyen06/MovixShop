aba7e7e9dd96f8906d9afd860a3acc4b6f17c50b
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-24 12:12:37 +0700

    fix bug connect db

--- backend/src/app/config/dbConnect.js
+++ backend/src/app/config/dbConnect.js
@@ -13,23 +13,3 @@ const dbConnect = async () => {
 };
 
 module.exports = dbConnect;
-
-// const mongoose = require("mongoose");
-
-// const dbConnect = async () => {
-//   try {
-//     const connection =
-//       process.env.MONGODB_URL &&
-//       (await mongoose.connect(process.env.MONGODB_URL));
-//     if (connection) {
-//       console.log("connect successfully");
-//     }
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       console.log(`Error: ${error.message}`);
-//     }
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = dbConnect;
